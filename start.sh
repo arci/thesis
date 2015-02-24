8ef96e8de514c6d534dae9cd77ad2d8e9f7fc2ac
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2015-02-24 21:21:46 +0100

    script to start working faster :)

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+osascript <<EOD
+  tell application "texmaker"  	open "Users:Fabio:Documents:Projects:Tesi:thesis:thesis.tex"  end tell  tell application "System Events" to tell process "texmaker"  	click menu item "Define Current Document as 'Master Document'" of menu 1 of menu bar item "Options" of menu bar 1  	click menu item "Full Screen" of menu 1 of menu bar item "View" of menu bar 1  end tell
+EOD
